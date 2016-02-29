8b0b7f61cc40994777c5135e8e9c216638f32644
Zheng  Weihan <zhengweihan.91@gmail.com>
2016-03-01 00:01:54 +0800

    add html to index.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>frontend</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800,300italic,400italic,600italic,700italic,800italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="frontend/dist/styles/main.css">
+        
+        <!-- build:js scripts/vendor/modernizr.js -->
+        <script src="frontend/dist/bower_components/modernizr/modernizr.js"></script>
+        <!-- endbuild -->
+        
+    </head>
+    <body>
+        <!--[if lt IE 10]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        
+        <div id="content">
+        </div>
+        
+
+        <!-- build:js scripts/vendor.js -->
+        <!-- bower:js -->
+        <script src="frontend/dist/bower_components/jquery/dist/jquery.js"></script>
+        
+        <script src="frontend/dist/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.min.js"></script>
+        
+        <!-- endbower -->
+        <!-- endbuild -->
+        <script src="frontend/dist/scripts/app.js"></script>
+
+    </body>
+</html>
