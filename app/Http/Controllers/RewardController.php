e86abcf97caff5340a82fd9fbbb67482f472b935
Sam-Mauris Yong <mauris@hotmail.sg>
2016-03-30 16:41:58 +0800

    moved to correct controller

--- app/Http/Controllers/RewardController.php
+++ app/Http/Controllers/RewardController.php
@@ -9,12 +9,4 @@ use App\Http\Controllers\Controller;
 
 class RewardController extends Controller
 {
-  public function store(Request $request, $transaction) {
-    $results = DB::insert("INSERT INTO \"Receipt\" (receiptNo, address, amount, transaction) VALUES (random_string(12), :address, :amount, :transaction)", [
-      'address'  => $request->input('address'),
-      'amount' => $request->input('amount'),
-      'transaction' => $transaction
-    ]);
-    return response()->json($results);
-  }
 }
