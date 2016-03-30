e361d3acd2edf49c4ba2d6ea41ac804a9ef5493a
Sam-Mauris Yong <mauris@hotmail.sg>
2016-03-30 16:42:12 +0800

    updated transaction controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -6,18 +6,22 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use ReceiptController;
+use DB;
 
 class TransactionController extends Controller
 {
+  /**
+   * URL route for creating a new user
+   * POST /api/transaction
+   */
   public function store(Request $request) {
-    DB::statement('BEGIN TRANSACTION;');
+    DB::statement('BEGIN TRANSACTION');
     $results = DB::insert("INSERT INTO \"transaction\" (code, type, \"user\") VALUES (random_string(12), :type, :user) RETURNING code", [
       'type'  => 'Credit',
       'user' => $request->input('user')
     ]);
-    ReceiptController::store($request);
-    DB::statement('ROLLBACK;');
+    //ReceiptController::store($request);
+    DB::statement('ROLLBACK');
     return response()->json($results);
   }
 }
