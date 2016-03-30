2fac1768198d1444f16f0ed2e18d847b5f1a99e1
Sam-Mauris Yong <mauris@hotmail.sg>
2016-03-30 12:06:40 +0800

    updated DDL
    important

--- database/ddl.sql
+++ database/ddl.sql
@@ -1,6 +1,8 @@
+BEGIN TRANSACTION;
+
 -- The Main DDL for the website
 CREATE TABLE Project(
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	title VARCHAR(128) NOT NULL,
 	description TEXT,
 	country CHAR(2) NOT NULL,
@@ -48,7 +50,7 @@ CREATE TABLE Project_Image(
 );
 
 CREATE TABLE Project_Comment(
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	content TEXT NOT NULL,
 	posted TIMESTAMP,
 	member VARCHAR(32) REFERENCES "user"(username),
@@ -57,7 +59,7 @@ CREATE TABLE Project_Comment(
 );
 
 CREATE TABLE Project_News(
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	title VARCHAR(64) NOT NULL,
 	content TEXT NOT NULL,
 	posted TIMESTAMP,
@@ -66,7 +68,7 @@ CREATE TABLE Project_News(
 );
 
 CREATE TABLE News_Comment(
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	content TEXT NOT NULL,
 	posted TIMESTAMP,
 	member VARCHAR(32) REFERENCES "user"(username),
@@ -75,13 +77,32 @@ CREATE TABLE News_Comment(
 );
 
 CREATE TABLE Transaction(
-	id INTEGER PRIMARY KEY,
-	type VARCHAR(32) CHECK(type = 'Credit' OR type = 'Debit')
+	code CHAR(12) PRIMARY KEY,
+	type CHAR(6) CHECK(type = 'Credit' OR type = 'Debit')
 );
 
 CREATE TABLE Receipt(
-	id INTEGER PRIMARY KEY,
+	receiptNo CHAR(12) PRIMARY KEY,
 	address TEXT,
 	amount NUMERIC CHECK(amount > 0.0),
-	transaction INTEGER REFERENCES Transaction(id)
+	transaction CHAR(12) REFERENCES Transaction(code)
 );
+
+CREATE OR REPLACE FUNCTION random_string(length integer) returns text AS
+$$
+DECLARE
+  chars text[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
+  result text := '';
+  i integer := 0;
+BEGIN
+  if length < 0 then
+    raise exception 'Given length cannot be less than 0';
+  end if;
+  for i in 1..length loop
+    result := result || chars[1+random()*(array_length(chars, 1)-1)];
+  end loop;
+  return result;
+END;
+$$ language plpgsql;
+
+COMMIT;
