1ee7e8aebbfdb0639e6f7f758e5e8deff46aeeca
Sam-Mauris Yong <mauris@hotmail.sg>
2016-03-30 17:00:06 +0800

    renamed CommentController to ProjectCommentController

---
+++ app/Http/Controllers/ProjectCommentController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ProjectCommentController extends Controller
+{
+    /**
+     * URL route for fetching a user's projects
+     * GET /api/users/{userId}/projects
+     */
+    public function index($user) {
+      $results = DB::select("SELECT * FROM \"project\", \"project_owner\" WHERE project.id = project_owner.project AND project_owner.member = :username", [
+        'username' => $user
+      ]);
+      return response()->json($results);
+    }
+
+}
