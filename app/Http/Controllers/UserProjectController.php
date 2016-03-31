d0c599244e786441bf448f4e2182f6246c6ace2d
Sam-Mauris Yong <mauris@hotmail.sg>
2016-03-31 15:02:41 +0800

    updated user projects to project only ongoing and complete projects

--- app/Http/Controllers/UserProjectController.php
+++ app/Http/Controllers/UserProjectController.php
@@ -15,7 +15,7 @@ class UserProjectController extends Controller
      * GET /api/user/{userId}/project
      */
     public function index($user) {
-      $results = DB::select("SELECT * FROM \"project\", \"project_owner\" WHERE project.id = project_owner.project AND project_owner.member = :username", [
+      $results = DB::select("SELECT * FROM \"project\", \"project_owner\" WHERE project.id = project_owner.project AND project_owner.member = :username AND (project.status = 'ONGOING' OR project.status = 'COMPLETE')", [
         'username' => $user
       ]);
       return response()->json($results);
