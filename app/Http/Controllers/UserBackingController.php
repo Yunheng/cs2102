c9899682ae257d30b47471c8d08a7562e748a26d
Sam-Mauris Yong <mauris@hotmail.sg>
2016-04-02 10:33:27 +0800

    add user backing controller

---
+++ app/Http/Controllers/UserBackingController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use DB;
+
+class UserBackingController extends Controller
+{
+    /**
+     * URL route for fetching all projects backed by a user
+     * GET /api/user/{userId}/backing
+     */
+    public function index($user) {
+      $results = DB::select("SELECT p.* FROM project_backer as b, project as p WHERE b.project = p.id AND b.member = :username ORDER BY p.date_created DESC, [
+        'username' => $user
+      ]);
+      return response()->json($results);
+    }
+}
