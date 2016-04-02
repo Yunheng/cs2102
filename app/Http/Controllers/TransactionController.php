7a17e58ea34cbb6447fe754490b90ddec55e89dc
Zheng  Weihan <zhengweihan.91@gmail.com>
2016-04-02 14:13:12 +0800

    fix bug

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -15,14 +15,12 @@ class TransactionController extends Controller
    * POST /api/transaction
    */
   public function store(Request $request) {
-    DB::statement('BEGIN TRANSACTION');
-    $results = DB::select("INSERT INTO \"transaction\" (code, type, \"user\") VALUES (random_string(12), :type, :user) RETURNING code", [
+    $results = DB::select("INSERT INTO \"transaction\" (code, type, amount, \"user\") VALUES (random_string(12), :type, :amount, :user) RETURNING code", [
       'type'  => 'Credit',
       'amount' => $request->input('amount'),
       'user' => $request->input('user')
     ]);
     app('App\Http\Controllers\ReceiptController')->store($request, $results[0]->code);
-    DB::statement('COMMIT');
     return response()->json($results[0]);
   }
 
